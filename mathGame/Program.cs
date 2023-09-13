e6267cfb3c8b3ea8e933ac60c733cefd10366f92
Milana <milana.zaborovska@gmail.com>
2023-09-13 21:55:21 +0100

    Written out intro of game

--- mathGame/Program.cs
+++ mathGame/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type your name");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division 
+Q - Quit the progress");
+Console.WriteLine("-------------------------------------------");
+
+Console.ReadLine();
