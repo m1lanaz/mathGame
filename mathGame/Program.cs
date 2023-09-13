8b789d791e27dd0cc86f1ec08d5c8be67f2f781f
Milana <milana.zaborovska@gmail.com>
2023-09-13 22:13:21 +0100

    Added if statement for selected game

--- mathGame/Program.cs
+++ mathGame/Program.cs
@@ -13,4 +13,29 @@ D - Division
 Q - Quit the progress");
 Console.WriteLine("-------------------------------------------");
 
-Console.ReadLine();
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.ToUpper().Trim() == "A")
+{
+    Console.WriteLine("IT WORKED!");
+}
+else if (gameSelected.ToUpper().Trim() == "S")
+{
+    Console.WriteLine("You chose S");
+}
+else if (gameSelected.ToUpper().Trim() == "M")
+{
+    Console.WriteLine("You chose M");
+}
+else if (gameSelected.ToUpper().Trim() == "D")
+{
+    Console.WriteLine("You chose D");
+}
+else if (gameSelected.ToUpper().Trim() == "Q")
+{
+    Console.WriteLine("You chose Q");
+}
+else
+{
+    Console.WriteLine("Please enter in a valid letter");
+};
