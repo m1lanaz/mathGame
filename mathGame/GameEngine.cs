ce437f10ba208296273f3a9b99a41a1157e99c43
Milana <milana.zaborovska@gmail.com>
2023-09-15 08:36:19 +0100

    complete game

--- mathGame/GameEngine.cs
+++ mathGame/GameEngine.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace mathGame
 {
     internal class GameEngine
@@ -49,12 +51,88 @@ namespace mathGame
 
         internal void SubtractionGame()
         {
-            Console.WriteLine("You chose S");
+            Console.Clear();
+            Console.WriteLine("Thank you for picking the subtraction game!");
+
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+
+                //int.parse converts the string input into an integer
+                if (int.Parse(result) == firstNumber - secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct!  Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer is incorrect!  Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Subtraction");
+
+            Console.WriteLine($"The game is over! Your score is {score}");
+            Console.WriteLine("Press any key to go back to main menu");
+            Console.ReadLine();
         }
 
         internal void MultiplicationGame()
         {
-            Console.WriteLine("You chose M");
+            Console.Clear();
+            Console.WriteLine("Thank you for picking the multiplication game!");
+
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+
+                //int.parse converts the string input into an integer
+                if (int.Parse(result) == firstNumber * secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct!  Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer is incorrect!  Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Multiplication");
+
+            Console.WriteLine($"The game is over! Your score is {score}");
+            Console.WriteLine("Press any key to go back to main menu");
+            Console.ReadLine();
         }
 
         internal void DivisionGame()
